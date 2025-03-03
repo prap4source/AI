e55c07eedcfc1867ff307778a6290ac2efe6ddb1
pradeepgopanapalli <25631922+prap4source@users.noreply.github.com>
2025-03-02 19:51:56 -0800

    Add placeholder for strategies

--- StockBotChat/Strategies.py
+++ StockBotChat/Strategies.py
@@ -3,3 +3,5 @@ import streamlit as st
 def show_strategies(st):
     st.title("Strategies")
     st.write("Get Stock trading strategies.")
+    strategy_choice = st.selectbox("Select a Strategy", ["Moving Average Crossover", "RSI Strategy", "MACD Strategy"])
+    st.write(f"Selected Strategy: {strategy_choice}")
